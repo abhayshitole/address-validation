3df2a62f9c1db597388768f6253f5d9d732bbdea
AbhayShitole <abhayshitole@gmail.com>
2022-11-21 13:30:06 +0530

    refactoring the class name

--- src/main/java/com/dataeconomy/masterdatamanagement/MasterDataManagementApplication.java
+++ src/main/java/com/dataeconomy/masterdatamanagement/MasterDataManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MasterdatamanagementApplication {
+public class MasterDataManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MasterdatamanagementApplication.class, args);
+		SpringApplication.run(MasterDataManagementApplication.class, args);
 	}
 
 }
